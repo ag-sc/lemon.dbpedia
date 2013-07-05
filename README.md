ea3aef981791f7cf3806e4de947cbabc268deb03
cunger <f060047@gmail.com>
2013-07-05 15:51:40 +0200

    mend

--- README.md
+++ README.md
@@ -7,16 +7,27 @@
 $ ./export.sh en
 ```
 
-*Requires:*
+Requires:
 
 * <a href="https://github.com/jmccrae/lemon.patterns">lemon.patterns</a>
 * Python with <a href="https://github.com/RDFLib/rdflib">rdflib</a>
 
 ## Latest release
 
-None yet.
+**Version 1 (July 2013)**
+
+The first release of the English lexicon for DBpedia 3.8 covers 353 classes 
+as well as 300 properties (all those that have more than 10,000 occurrences in the DBpedia dataset, 
+with only a few exceptions).
+
+* Total lexicalizations: 1,216 (1.8 entries per concept)
+* Class lexicalizations: 443 (1.3 entries per class)
+* Property lexicalizations: 773 (2.4 entries per property)
+
+Published on: <a href="http://lemon-model.net/">lemon-model.net</a>
+
 
 ## Want to contribute?
 
-Yes, please!
-
+If you want to help to improve and extend the lexicon, if you want to port it to others languages,
+ or if you are using the lexicon, we'd love to hear from you!
---
+++ statistics.py
@@ -0,0 +1,97 @@
+from __future__ import division
+import rdflib as rdf
+import re
+
+
+def frag_uri(uri):
+    if not ':/' in str(uri): return str(uri)
+    else: return re.match('.*/([^/]+\#)?([^/]+)$',str(uri)).group(2)
+
+
+def run():
+
+  lexicon = 'target/dbpedia_en.rdf'
+
+  graph = rdf.Graph()
+  graph = graph.parse(lexicon)
+
+
+  dontcount = ['propertyDomain','propertyRange','someValuesFrom','first']
+
+  number_cs = dict()
+  number_ps = dict()
+
+  uris = []
+  for line in open('en_lexicalizedURIs','r').readlines():
+      uris.append(line.strip())
+      if line.strip()[0].isupper():
+         number_cs[line.strip()] = 0
+      else:
+         number_ps[line.strip()] = 0
+
+  for s,p,o in graph:
+      if str(o).startswith('http://dbpedia.org'):
+         uri = frag_uri(o) 
+         skip = False
+         for dont in dontcount:
+             if dont in str(p): 
+                skip = True
+                break
+         if uri in uris and not skip:
+            if number_cs.has_key(uri):
+               number_cs[uri] = number_cs[uri] + 1
+            if number_ps.has_key(uri):
+               number_ps[uri] = number_ps[uri] + 1
+
+  print 'COUNTS \n' 
+  print '==== Classes ===================\n'
+  for k in number_cs.keys():
+      print k + ': ' + str(number_cs[k])
+  print '==== Properties ================\n'
+  for k in number_ps.keys():
+      print k + ': ' + str(number_ps[k])
+
+  distribution_cs = dict()
+  distribution_ps = dict()
+  
+  for k in number_cs.keys():
+      v = number_cs[k]
+      if distribution_cs.has_key(v):
+         distribution_cs[v] = distribution_cs[v] + 1
+      else:
+         distribution_cs[v] = 1
+  for k in number_ps.keys():
+      v = number_ps[k]
+      if distribution_ps.has_key(v):
+         distribution_ps[v] = distribution_ps[v] + 1
+      else:
+         distribution_ps[v] = 1
+
+  c_lex = 0
+  p_lex = 0
+  for k,v in distribution_cs.items():
+      c_lex += k*v
+  for k,v in distribution_ps.items():
+      p_lex += k*v
+
+  print '\n\n---------------------------------------------'
+  print 'Total    lexicalizations: ' + str(c_lex+p_lex) + ' (' + str((c_lex+p_lex)/(len(number_cs)+len(number_ps))) + ' per concept)'
+  print 'Class    lexicalizations: ' + str(c_lex) + ' (' + str(c_lex/len(number_cs)) + ' per class)'
+  print 'Property lexicalizations: ' + str(p_lex) + ' (' + str(p_lex/len(number_ps)) + ' per property)'
+  print '---------------------------------------------'
+
+  print '\n'
+  print 'DISTRIBUTION \n' 
+  print '==== Classes ===================\n'
+  for k in distribution_cs.keys():
+      print str(k) + ': ' + str(distribution_cs[k])
+  print '==== Properties ================\n'
+  for k in distribution_ps.keys():
+      print str(k) + ': ' + str(distribution_ps[k])
+
+
+## MAIN #########
+
+if __name__ == "__main__":
+   run()
+
